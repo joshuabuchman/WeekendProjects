722dbbd3535a18e52d019fb78af30ad703300ce0
Joshua Buchman <joshua.buchman@msn.com>
2020-01-23 18:58:35 -0500

    isMatch add

---
+++ In class work/isMacth.js
@@ -0,0 +1,22 @@
+//write the isMatch function
+//isMatch takes two objects
+//if the first object has all the properties of the second object return true
+//other wise return false
+//isMatch({ foo: true, bar: 1}, { foo: true });//true
+//isMatch({ foo: 42, bazz: 19}, {});//true
+//isMatch({ foo: 42, bazz: 11}, { foo: 43 });//false
+//isMatch({ }, { foo: 11 });//false
+
+const isMatch = (obj1, obj2) =>
+{
+    const entries2 = Object.entries(obj2);
+    for(let i=0; i<entries2.length; i++)
+    {
+        const [key,value] = entries2[i];
+        if(obj1[key]!=value)
+        {
+            return false;
+        }
+    }
+    return true;
+}
